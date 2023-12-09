4180dd84ac7735f21ffd2e1de7d90cf9f4b7a3a1
nellichou <153381802+nellichou@users.noreply.github.com>
2023-12-09 17:45:34 +0100

    Create types.h

---
+++ types.h
@@ -0,0 +1,13 @@
+/* types.h 
+contient les definitions des types utilisés*/
+typedef struct {
+    int gr[62][42];
+    int tete_snake[2]; /* coordonnes tete snake 1er: X , 2eme Y*/
+    int queue_snake[2]; /* coordonnes queue du snake*/
+    int direction; /* direction du snake 0=haut, 1=bas, 2=gauche, 3=droite*/
+    int collision;
+    int score;
+    char scoreTxt[10];
+    int nb_pommes;
+    unsigned long cycle; /*Le cycle est la vitesse du snake. Plus il est bas, plus il va vite*/
+} grille;
