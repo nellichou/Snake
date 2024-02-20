b78517378748e415f9b25a72bc31a47b4edefa96
nellichou <153381802+nellichou@users.noreply.github.com>
2024-02-20 18:02:54 +0100

    Update types.h

--- types.h
+++ types.h
@@ -1,6 +1,8 @@
 /* types.h 
 contient les definitions des types utilisés*/
+
 typedef struct {
+
     int gr[62][42];
     int tete_snake[2]; /* coordonnes tete snake 1er: X , 2eme Y*/
     int queue_snake[2]; /* coordonnes queue du snake*/
@@ -10,4 +12,6 @@ typedef struct {
     char scoreTxt[10];
     int nb_pommes;
     unsigned long cycle; /*Le cycle est la vitesse du snake. Plus il est bas, plus il va vite*/
+
 } grille;
+
