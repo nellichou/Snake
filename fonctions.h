b212b97bdef4b477bfdb2fa7b6fa7b7e27a5084d
nellichou <153381802+nellichou@users.noreply.github.com>
2024-02-20 18:02:21 +0100

    Update fonctions.h

--- fonctions.h
+++ fonctions.h
@@ -2,8 +2,8 @@
 contient les prototypes
 */
 #include "types.h"
+
 void initGrille(grille* g); /* affichage grille initiale*/
 void afficheGrille(grille* g, int pomme, int tete, int terrain); /* affichage nouvelle grille*/
-void afficheGrilleTxt(grille* g); /* affichage grille dans terminal*/
 void majGrille(grille* g, int touche); /*mise à jour grille*/
-void ecranPrincipal(char* c, char* s);
+void ecranPrincipal(char* c, char* s,char* x);
